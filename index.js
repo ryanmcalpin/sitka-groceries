3474d1692e803457484871e3f556e39e9ec8a3f5
Ryan McAlpin <rhmcalpin@gmail.com>
2018-04-13 18:56:03 -0700

    convert rl.question to switch case, use recursion on invalid option, move front-end logic to bottom

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const readline = require('readline');
 
-console.log('Welcome to Sitka Groceries!');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('Choose an option below.\n1) Shop\n2) View Cart\n3) Checkout\n4) Exit\nYour Choice: ', (answer) => {
-  // TODO: Log the answer in a database
-  // console.log(`Thank you for your valuable feedback: ${answer}`);
-  if (answer == "4") {
-    console.log("Goodbye!");
-    rl.close();
-  }
+var displayMainMenu = function () {
+  rl.question('Choose an option below.\n1) Shop\n2) View cart\n3) Checkout\n4) Exit\nYour choice: ', (answer) => {
+    switch(answer) {
+      case "1":
+      break;
+      case "2":
+      break;
+      case "3":
+      break;
+      case "4":
+      console.log("Goodbye!");
+      rl.close();
+      break;
+      default:
+      console.log("Invalid option!");
+      displayMainMenu();
+    }
+  });
+}
 
-});
+// front-end
+console.log('Welcome to Sitka Groceries!');
+displayMainMenu();
